da90bbcf0fdbc1b2d9fd0751068d5341f4d0e317
MrLeafster <starwarsjedobar@gmail.com>
2022-11-15 17:15:44 +0100

    Little final code cleanup :-)

--- WESHackaton/src/RPMHandler.cpp
+++ WESHackaton/src/RPMHandler.cpp
@@ -3,17 +3,6 @@
 #include "BregHandler.h"
 #include "BobHandler.h"
 
-
-uint32_t past_delta;
-uint32_t delta;
-
-uint32_t past_delta_sum;
-uint32_t delta_sum;
-
-#define ZUBAC_ANGLE 15.0
-#define ZUBAC1_ANGLE 195.0
-#define INC_DELAY 124
-
 int zubac_count = 0;
 bool is_toothless = false;
 bool is_on_first_rotation = false;
@@ -45,31 +34,30 @@ float calculateSpark(){
 	
 	int _zubac_count = zubac_count;
     interrupts();
-	// uint32_t _past_delta = past_delta / 16.0; // number of time that the timer register overflowed
 	
 	unsigned int _delay;
-
     float current_rpm = getRPM();
 
     int value = analogRead(pinMAP);
     float angle = getAngle(fmap((float)value, 102, 920, 15, 100), current_rpm);
 
+    // ----NEW WAY----
     float tp = timeFromAngle(angle);
     float perAngle = angleFromTime(1500.0f + tp);
     float beta = 360 - ZUBAC1_ANGLE - perAngle;
     _delay = timeFromAngle(beta);
-    // Serial.println(_delay);
 
-    //  = (360 - angle - (1500 * 15.0 / _past_delta) - current_position) * _past_delta / 15.0;
+    return _delay;
 
+    // ----OLDER WAY----
+    //  uint32_t _past_delta = past_delta / 16.0; // number of time that the timer register overflowed
+    //  delay = (360 - angle - (1500 * 15.0 / _past_delta) - current_position) * _past_delta / 15.0;
+
+    // ----LITTLE LESS OLDER WAY----
     // float micro_spr_360 = 60.0 / current_rpm * 1000000.0 / 360;
     // _delay = (360 - angle - 1.0 / micro_spr_360 * 1500 - current_position) * micro_spr_360;
 
-
 	// delayMicroseconds(_delay - INC_DELAY);  // 1.5ms corrected for instruction
-	
-
-    return _delay;
 }
 
 ISR(TIMER1_OVF_vect)
@@ -97,10 +85,10 @@ ISR(TIMER1_CAPT_vect){
 
         delta = current_time - last_time_read;
 
-        if(past_delta * 2 < delta){       //end of cycle
+        if(past_delta * 2 < delta){                      // end of cycle
             past_delta_sum = delta_sum + delta;
             
-            delta_sum = 0;                  // NGL fix
+            delta_sum = 0;                               // NGL fix
             past_delta = 0;
 
             is_toothless = true;
@@ -114,8 +102,8 @@ ISR(TIMER1_CAPT_vect){
     
     firstRisingPassed = true;
 
-    if(bregChecked){  // provijeri je li proso kalibracijski brijeg
-        if(is_toothless){  // provijeri je li trenutni uzlazni brid dolazi nakon rupe koljenstog vratila
+    if(bregChecked){                                      // provijeri je li proso kalibracijski brijeg
+        if(is_toothless){                                 // provijeri je li trenutni uzlazni brid dolazi nakon rupe koljenstog vratila
             zubac_count += 3;
             zubac_count = zubac_count % 24;
             
@@ -123,13 +111,11 @@ ISR(TIMER1_CAPT_vect){
             treashold++;
 
             if(treashold > 4) bregExists = false;
-        
+
+            is_toothless = false;
         } else {
             zubac_count += 1;
         }
-
-        is_toothless = false;
-
 
         if(zubac_count == 1){
             if(is_on_first_rotation || !bregExists){
@@ -143,32 +129,32 @@ ISR(TIMER1_CAPT_vect){
                 if(!firstBreg) calculateSpark();
             } 
         }
-        
     }
 }
 
 void setupRPMTimer() {
     noInterrupts ();  // protected code
+    
     // reset Timer 1
     TCCR1A = 0;
     TCCR1B = 0;
     TCNT1 = 0;
     TIMSK1 = 0;
 
-    TIFR1 |= _BV(ICF1); // clear Input Capture Flag so we don't get a bogus interrupt
-    TIFR1 |= _BV(TOV1); // clear Overflow Flag so we don't get a bogus interrupt
+    TIFR1 |= _BV(ICF1);   // clear Input Capture Flag so we don't get a bogus interrupt
+    TIFR1 |= _BV(TOV1);   // clear Overflow Flag so we don't get a bogus interrupt
 
-    TCCR1B = _BV(CS10) | // start Timer 1, no prescaler
-            _BV(ICES1); // Input Capture Edge Select (1=Rising, 0=Falling)
+    TCCR1B = _BV(CS10) |  // start Timer 1, no prescaler
+            _BV(ICES1);   // Input Capture Edge Select (1=Rising, 0=Falling)
 
     TIMSK1 |= _BV(ICIE1); // Enable Timer 1 Input Capture Interrupt
     TIMSK1 |= _BV(TOIE1); // Enable Timer 1 Overflow Interrupt
 
-    past_delta = 0;
-    delta = 0;
-
+    past_delta     = 0;
+    delta          = 0;
     past_delta_sum = 0;
-    delta_sum = 0;
+    delta_sum      = 0;
+    
     interrupts ();
 }
 
